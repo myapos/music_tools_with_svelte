74be310cbfdf3dbd02ccc7aaee94ad9de8348a66
myapos <myapos@gmail.com>
2022-08-23 00:39:55 +0300

    added spinner and color in body

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,33 @@
 <script>
 	import '../app.css';
+	import { Circle } from 'svelte-loading-spinners';
 	import Menu from '$lib/components/Menu.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import { onMount } from 'svelte';
+
+	let isLoading = true;
+	onMount(() => {
+		isLoading = false;
+	});
 </script>
 
 <svelte:head>
 	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
 </svelte:head>
 
-<Menu />
+{#if isLoading}
+	<main class="flex justify-center items-center w-full h-screen flex-col">
+		<Circle size="60" color="#FF3E00" unit="px" duration="1s" />
+	</main>
+{:else}
+	<Menu />
 
-<main class="flex justify-center items-center flex-col">
-	<slot />
-</main>
+	<main class="flex justify-center items-center flex-col">
+		<slot />
+	</main>
 
-<Footer />
+	<Footer />
+{/if}
 
 <style>
 	main {
@@ -25,5 +38,6 @@
 		/* this will apply to <body> */
 		height: 100vh;
 		font-family: Roboto;
+		background-color: #2563eb30;
 	}
 </style>
