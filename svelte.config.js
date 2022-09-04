f8b43af01c1d9c5afb5a3a65c11d18fad15488dc
myapos <myapos@gmail.com>
2022-09-04 12:06:44 +0300

    reenabled static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,15 +13,15 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter(),
-		// adapter: adapter({
-		// 	// default options are shown. On some platforms
-		// 	// these options are set automatically — see below
-		// 	pages: 'build',
-		// 	assets: 'build',
-		// 	fallback: null,
-		// 	precompress: false
-		// }),
+		// adapter: adapter(),
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
 		prerender: {
 			default: true
 		}
