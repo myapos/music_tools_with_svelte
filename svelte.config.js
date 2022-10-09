7780ca382ddaa7239b8b5cc82340adb7d586d7cc
myapos <myapos@gmail.com>
2022-10-09 22:20:53 +0300

    use adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,18 +13,18 @@ const config = {
 	],
 
 	kit: {
-		// adapter: adapter(),
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'public',
-			assets: 'public',
-			fallback: true,
-			precompress: false
-		})
-		// prerender: {
-		// 	default: true
-		// }
+		adapter: adapter(),
+		// adapter: adapter({
+		// 	// default options are shown. On some platforms
+		// 	// these options are set automatically — see below
+		// 	pages: 'public',
+		// 	assets: 'public',
+		// 	fallback: null,
+		// 	precompress: false
+		// }),
+		prerender: {
+			default: true
+		}
 	}
 };
 
