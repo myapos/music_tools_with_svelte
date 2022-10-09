aa80e0f3ea865bc91cc4a35d50a862657f1946e0
myapos <myapos@gmail.com>
2022-10-09 22:16:58 +0300

    fallback to true

--- svelte.config.js
+++ svelte.config.js
@@ -19,7 +19,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'public',
 			assets: 'public',
-			fallback: null,
+			fallback: true,
 			precompress: false
 		})
 		// prerender: {
